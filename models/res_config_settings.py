a5d47f1640f28a9aaccb444b23c3fc6f90b9fa8f
AhmedAabedd <ahmed.labedd@gmail.com>
2025-08-04 19:38:01 +0100

    Add menu visibility based on config settings

--- models/res_config_settings.py
+++ models/res_config_settings.py
@@ -15,3 +15,17 @@ class ResConfigSettings(models.TransientModel):
         help="Activate or deactivate the category promotions system",
         config_parameter='category_promotion.enable_promotions'
     )
+
+    @api.model
+    def set_values(self):
+        super().set_values()
+
+        # Reference the menus (set raise_if_not_found = False to avoid crash)
+        menu_main = self.env.ref('category_promotion.menu_promotion', raise_if_not_found=False)
+
+        # Read setting value
+        is_active = self.category_promotion_active
+
+        # Set menu visibility
+        if menu_main:
+            menu_main.active = is_active
